96e73f09279bdd9aceb50687d2bde5c28a4b6471
Jason Green <jason@green.io>
2018-10-15 12:56:23 -0400

    this is what I run

---
+++ go.sh
@@ -0,0 +1,3 @@
+cd /home/pi/PaperTTY+pyte/PaperTTY/
+/usr/bin/python3 /home/pi/PaperTTY+pyte/PaperTTY/papertty.py --driver epd2in13 ptycommand --command "tmux a -t tty" --autofit --scrub --font miniwi-fixed-8.bdf --spacing -1
+
